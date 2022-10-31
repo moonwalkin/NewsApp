30f8b4ccaf321ce9cce9fc958f4ddf0e8908e521
Artem <art.samokysh@gmail.com>
2022-10-31 23:52:34 +0200

    add navigation

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigator {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+           launch(NewsFragment(), false)
+        }
+    }
+
+    private fun launch(fragment: Fragment, addToBackStack: Boolean) {
+        val transaction = supportFragmentManager.beginTransaction()
+        val container = R.id.fragment_container
+        if (addToBackStack) {
+            transaction.replace(container, fragment)
+                .addToBackStack(null)
+        } else {
+            transaction.add(container, fragment)
+        }
+        transaction.commit()
+    }
+
+    override fun openArticle(article: String) {
+        launch(ArticleFragment.newInstance(article), true)
     }
 }
