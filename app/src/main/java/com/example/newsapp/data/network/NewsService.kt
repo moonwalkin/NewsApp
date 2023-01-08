b65e0346e0df8d62f512c19deaa717f2f0e22ac4
Artem <art.samokysh@gmail.com>
2023-01-08 11:41:10 +0200

    hide api key

--- app/src/main/java/com/example/newsapp/data/network/NewsService.kt
+++ app/src/main/java/com/example/newsapp/data/network/NewsService.kt
@@ -1,5 +1,6 @@
 package com.example.newsapp.data.network
 
+import com.example.newsapp.BuildConfig
 import com.example.newsapp.data.models.NewsData
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,13 +9,13 @@ interface NewsService {
 
     @GET("v2/top-headlines")
     suspend fun fetchTopNews(
-        @Query("apiKey") apiKey: String = "0c1198243f494b6ca3a0e6f0444e7e1c",
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY,
         @Query("country") country: String = "ua",
     ): NewsData
 
     @GET("v2/everything")
     suspend fun fetchByQuery(
         @Query("q") query: String,
-        @Query("apiKey") apiKey: String = "0c1198243f494b6ca3a0e6f0444e7e1c",
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY,
     ): NewsData
 }
